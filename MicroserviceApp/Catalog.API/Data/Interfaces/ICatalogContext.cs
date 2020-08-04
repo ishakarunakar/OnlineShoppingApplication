a0e1a206f6fd151a19232f369a74abba5e0006cc
PriyankaAryan <50867363+PriyankaAryan@users.noreply.github.com>
2020-08-04 15:37:36 +0530

    Add files via upload

---
+++ MicroserviceApp/Catalog.API/Data/Interfaces/ICatalogContext.cs
@@ -0,0 +1,15 @@
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data.Interfaces
+{
+    public interface ICatalogContext
+    {
+         IMongoCollection<Product> Products{get;}
+    }
+}
+
+
+
+
+
